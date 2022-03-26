462d724a9d023b88bb71e55e2cddb0397e1cbfe3
impasta21 <mlvnsackey@gmail.com>
2022-03-25 22:45:14 -0400

    clean up

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -25,7 +25,7 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
-    RobotContainer.m_DriveTrain.setDefaultCommand(RobotContainer.m_Drive);
+    m_robotContainer.m_DriveTrain.setDefaultCommand(RobotContainer.m_Drive);
   }
 
   /**
